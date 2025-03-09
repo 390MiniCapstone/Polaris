a44f43b1ea3350c10003408f36982cca7b4316eb
Omar Ziad <o.ziad@outlook.com>
2025-03-09 18:17:09 -0400

    Run prettier to fix formatting

--- Polaris-FE/components/ColorblindButton.tsx
+++ Polaris-FE/components/ColorblindButton.tsx
@@ -1,132 +1,132 @@
-import React, {useState} from 'react';
-import {Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-
+import React, { useState } from 'react';
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface ColorblindButtonProps {
-    setColorblindTheme: (value: string) => void;
+  setColorblindTheme: (value: string) => void;
 }
 
-export function ColorblindButton({setColorblindTheme}: ColorblindButtonProps) {
-    const [modalVisible, setModalVisible] = useState(false);
-    const toggleModal = () => {
-        setModalVisible(!modalVisible);
-    };
+export function ColorblindButton({
+  setColorblindTheme,
+}: ColorblindButtonProps) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
 
-    return (
-        <View>
-            <TouchableOpacity style={styles.button} onPress={toggleModal}>
-                <Text style={styles.buttonText}>🎨 Mode</Text>
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={toggleModal}>
+        <Text style={styles.buttonText}>🎨 Mode</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent
+        visible={modalVisible}
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Select a Colorblind Mode</Text>
+
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Default selected');
+                setColorblindTheme('');
+                toggleModal();
+              }}
+            >
+              <Text style={styles.optionText}>🟠 Default </Text>
             </TouchableOpacity>
 
-            <Modal
-                animationType="slide"
-                transparent
-                visible={modalVisible}
-                onRequestClose={toggleModal}
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Deuteranopia selected');
+                setColorblindTheme('deuteranopia');
+                toggleModal();
+              }}
             >
-                <View style={styles.modalOverlay}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>Select a Colorblind Mode</Text>
+              <Text style={styles.optionText}>🟢 Deuteranopia (Red-Green)</Text>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Protanopia selected');
+                setColorblindTheme('protanopia');
+                toggleModal();
+              }}
+            >
+              <Text style={styles.optionText}>🔴 Protanopia (Red-Green)</Text>
+            </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Default selected');
-                                setColorblindTheme('');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🟠 Default </Text>
-                        </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Tritanopia selected');
+                setColorblindTheme('tritanopia');
+                toggleModal();
+              }}
+            >
+              <Text style={styles.optionText}>🔵 Tritanopia (Blue-Yellow)</Text>
+            </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Deuteranopia selected');
-                                setColorblindTheme('deuteranopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🟢 Deuteranopia (Red-Green)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Protanopia selected');
-                                setColorblindTheme('protanopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🔴 Protanopia (Red-Green)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Tritanopia selected');
-                                setColorblindTheme('tritanopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🔵 Tritanopia (Blue-Yellow)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={[styles.optionButton, {backgroundColor: 'black'}]}
-                            onPress={toggleModal}
-                        >
-                            <Text style={[styles.optionText, {color: 'white'}]}>
-                                Cancel
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
+            <TouchableOpacity
+              style={[styles.optionButton, { backgroundColor: 'black' }]}
+              onPress={toggleModal}
+            >
+              <Text style={[styles.optionText, { color: 'white' }]}>
+                Cancel
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    );
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'rgba(34, 34, 34, 0.992)',
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderRadius: 8,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        width: '80%',
-        backgroundColor: 'rgba(34, 34, 34, 0.992)',
-        padding: 20,
-        borderRadius: 12,
-        alignItems: 'center',
-    },
-    modalTitle: {
-        color: 'white',
-        fontSize: 18,
-        fontWeight: '600',
-        marginBottom: 12,
-    },
-    optionButton: {
-        backgroundColor: '#8E8E8E',
-        padding: 10,
-        borderRadius: 8,
-        marginTop: 8,
-        width: '100%',
-        alignItems: 'center',
-    },
-    optionText: {
-        color: 'white',
-    },
+  button: {
+    backgroundColor: 'rgba(34, 34, 34, 0.992)',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: 'rgba(34, 34, 34, 0.992)',
+    padding: 20,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 12,
+  },
+  optionButton: {
+    backgroundColor: '#8E8E8E',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 8,
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
+    color: 'white',
+  },
 });
--- Polaris-FE/components/Map.tsx
+++ Polaris-FE/components/Map.tsx
@@ -1,65 +1,69 @@
 import React from 'react';
-import MapView, {Geojson, Region} from 'react-native-maps';
-import {StyleSheet, View} from 'react-native';
-import {downtownBuildings, loyolaBuildings} from '@/constants/buildings';
-import {Buildings} from './Buildings/Buildings';
-import {colorBlindMapStyle} from '@/constants/colorBlindMapStyle';
+import MapView, { Geojson, Region } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import { downtownBuildings, loyolaBuildings } from '@/constants/buildings';
+import { Buildings } from './Buildings/Buildings';
+import { colorBlindMapStyle } from '@/constants/colorBlindMapStyle';
 
 interface MapComponentProps {
-    mapRef: React.RefObject<MapView>;
-    region: Region | undefined;
-    setRegion: (region: Region) => void;
-    colorblindTheme: string;
+  mapRef: React.RefObject<MapView>;
+  region: Region | undefined;
+  setRegion: (region: Region) => void;
+  colorblindTheme: string;
 }
 
 export const MapComponent: React.FC<MapComponentProps> = ({
-                                                              mapRef,
-                                                              region,
-                                                              setRegion,
-                                                              colorblindTheme,
-                                                          }) => {
-    return (
-        <View style={styles.container}>
-            <MapView
-                testID="map-view"
-                ref={mapRef}
-                style={styles.map}
-                initialRegion={region}
-                onRegionChangeComplete={setRegion}
-                showsUserLocation
-                showsCompass
-                customMapStyle={colorblindTheme && colorBlindMapStyle[colorblindTheme] ? colorBlindMapStyle[colorblindTheme] : []}
-                tintColor="#A83B4A"
-            >
-                <Geojson
-                    geojson={downtownBuildings as GeoJSON.FeatureCollection}
-                    fillColor="rgba(143, 34, 54, 0.8)"
-                />
-                <Geojson
-                    geojson={loyolaBuildings as GeoJSON.FeatureCollection}
-                    fillColor="rgba(143, 34, 54, 0.8)"
-                />
-                <Buildings/>
-            </MapView>
-        </View>
-    );
+  mapRef,
+  region,
+  setRegion,
+  colorblindTheme,
+}) => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        testID="map-view"
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={region}
+        onRegionChangeComplete={setRegion}
+        showsUserLocation
+        showsCompass
+        customMapStyle={
+          colorblindTheme && colorBlindMapStyle[colorblindTheme]
+            ? colorBlindMapStyle[colorblindTheme]
+            : []
+        }
+        tintColor="#A83B4A"
+      >
+        <Geojson
+          geojson={downtownBuildings as GeoJSON.FeatureCollection}
+          fillColor="rgba(143, 34, 54, 0.8)"
+        />
+        <Geojson
+          geojson={loyolaBuildings as GeoJSON.FeatureCollection}
+          fillColor="rgba(143, 34, 54, 0.8)"
+        />
+        <Buildings />
+      </MapView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        width: '100%',
-        height: '100%',
-    },
-    buttonContainer: {
-        position: 'absolute',
-        top: 20,
-        alignSelf: 'center',
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10,
-        zIndex: 1,
-    },
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 20,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    zIndex: 1,
+  },
 });
