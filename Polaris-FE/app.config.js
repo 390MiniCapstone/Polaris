6c213c0cfea0b7a579c01282f695bd9111c7ea04
JayC-SF <srengfloresj@hotmail.com>
2025-03-09 03:51:34 -0400

    Setup conditional ms clarity on native build
    Co-authored-by: laraxl <laraxl@users.noreply.github.com>
    Co-authored-by: AsimRahman88 <AsimRahman88@users.noreply.github.com>

--- Polaris-FE/app.config.js
+++ Polaris-FE/app.config.js
@@ -55,7 +55,9 @@ export default {
       firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
       iosClientId: process.env.IOS_CLIENT_ID,
       androidClientId: process.env.ANDROID_CLIENT_ID,
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      maestroTest: process.env.MAESTRO_TEST,
+      msClarityApiKey: process.env.MS_CLARITY_API_KEY
     }
   }
 }
