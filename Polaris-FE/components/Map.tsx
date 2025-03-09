a3209e00dc11a17ba9593ddf5889a2ffd0af3dfe
Omar Ziad <o.ziad@outlook.com>
2025-03-09 17:34:02 -0400

    Add placeholder colorblind button that enables deuternopia map style

--- Polaris-FE/components/Map.tsx
+++ Polaris-FE/components/Map.tsx
@@ -1,57 +1,76 @@
-import React from 'react';
-import MapView, { Geojson, Region } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-import { downtownBuildings, loyolaBuildings } from '@/constants/buildings';
-import { Buildings } from './Buildings/Buildings';
+import React, {useState} from 'react';
+import MapView, {Geojson, Region} from 'react-native-maps';
+import {Button, StyleSheet, View} from 'react-native';
+import {downtownBuildings, loyolaBuildings} from '@/constants/buildings';
+import {Buildings} from './Buildings/Buildings';
+import {colorBlindMapStyle} from '@/constants/colorBlindMapStyle';
 
 interface MapComponentProps {
-  mapRef: React.RefObject<MapView>;
-  region: Region | undefined;
-  setRegion: (region: Region) => void;
+    mapRef: React.RefObject<MapView>;
+    region: Region | undefined;
+    setRegion: (region: Region) => void;
 }
 
 export const MapComponent: React.FC<MapComponentProps> = ({
-  mapRef,
-  region,
-  setRegion,
-}) => {
-  return (
-    <View>
-      <MapView
-        testID="map-view"
-        ref={mapRef}
-        style={styles.map}
-        initialRegion={region}
-        onRegionChangeComplete={setRegion}
-        showsUserLocation
-        showsCompass
-        tintColor="#A83B4A"
-      >
-        <Geojson
-          geojson={downtownBuildings as GeoJSON.FeatureCollection}
-          fillColor="rgba(143, 34, 54, 0.8)"
-        />
-        <Geojson
-          geojson={loyolaBuildings as GeoJSON.FeatureCollection}
-          fillColor="rgba(143, 34, 54, 0.8)"
-        />
-        <Buildings />
-      </MapView>
-    </View>
-  );
+                                                              mapRef,
+                                                              region,
+                                                              setRegion,
+                                                          }) => {
+    const [colorBlindMode, setColorBlindMode] = useState(false);
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                testID="map-view"
+                ref={mapRef}
+                style={styles.map}
+                initialRegion={region}
+                onRegionChangeComplete={setRegion}
+                showsUserLocation
+                showsCompass
+                customMapStyle={colorBlindMode ? colorBlindMapStyle.deuteranopia : []}
+                tintColor="#A83B4A"
+            >
+                <Geojson
+                    geojson={downtownBuildings as GeoJSON.FeatureCollection}
+                    fillColor="rgba(143, 34, 54, 0.8)"
+                />
+                <Geojson
+                    geojson={loyolaBuildings as GeoJSON.FeatureCollection}
+                    fillColor="rgba(143, 34, 54, 0.8)"
+                />
+                <Buildings/>
+            </MapView>
+
+            <View style={styles.buttonContainer}>
+                <Button
+                    title={
+                        colorBlindMode
+                            ? 'Disable Colorblind Mode'
+                            : 'Enable Colorblind Mode'
+                    }
+                    onPress={() => setColorBlindMode(!colorBlindMode)}
+                />
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 20,
-    alignSelf: 'center',
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 10,
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+    buttonContainer: {
+        position: 'absolute',
+        top: 20,
+        alignSelf: 'center',
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        zIndex: 1,
+    },
 });
