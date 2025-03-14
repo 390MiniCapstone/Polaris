b590eb4906b1117cbd314d982c570f347a5773d2
Omar Ziad <o.ziad@outlook.com>
2025-03-13 22:41:05 -0400

    Revert "Add unit tests for MapComponent and ColorblindButton"
    This reverts commit 0d1d684bba70ae8362adfb71f5f5b7a164b9e2ff.

--- Polaris-FE/components/__tests__/Map.test.tsx
+++ Polaris-FE/components/__tests__/Map.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import MapView from 'react-native-maps';
 import { MapComponent } from '@/components/Map';
 
@@ -20,7 +20,6 @@ describe('MapComponent', () => {
         mapRef={mockMapRef}
         region={mockRegion}
         setRegion={mockSetRegion}
-        colorblindTheme={''}
       />
     );
     expect(getByTestId('map-view')).toBeTruthy();
@@ -32,7 +31,6 @@ describe('MapComponent', () => {
         mapRef={mockMapRef}
         region={mockRegion}
         setRegion={mockSetRegion}
-        colorblindTheme={''}
       />
     );
 
@@ -46,24 +44,8 @@ describe('MapComponent', () => {
         mapRef={mockMapRef}
         region={mockRegion}
         setRegion={mockSetRegion}
-        colorblindTheme={''}
       />
     );
     expect(mockMapRef.current).toBeInstanceOf(MapView);
-  });
-
-  it('applies the correct colorblind map style', () => {
-    const { getByTestId } = render(
-      <MapComponent
-        mapRef={mockMapRef}
-        region={mockRegion}
-        setRegion={mockSetRegion}
-        colorblindTheme="deuteranopia"
-      />
-    );
-
-    const mapView = getByTestId('map-view');
-
-    expect(mapView.props.customMapStyle).toBeDefined();
   });
 });
