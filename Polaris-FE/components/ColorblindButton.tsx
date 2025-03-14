7191bbe8f374acfe1c9949bd65ddfa408c6cffb7
Omar Ziad <o.ziad@outlook.com>
2025-03-13 22:41:35 -0400

    Revert "Update map colorblind theme depending on user selection"
    This reverts commit e12dac8911bc251798034569b9383de9abf8b15b.

--- Polaris-FE/components/ColorblindButton.tsx
+++ Polaris-FE/components/ColorblindButton.tsx
@@ -1,132 +1,117 @@
-import React, {useState} from 'react';
-import {Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
 
+/**
+ * A self-contained button that opens a modal to select colorblind modes.
+ * All logic & state are inside this component.
+ */
+export function ColorblindButton() {
+  const [modalVisible, setModalVisible] = useState(false);
 
-interface ColorblindButtonProps {
-    setColorblindTheme: (value: string) => void;
-}
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
 
-export function ColorblindButton({setColorblindTheme}: ColorblindButtonProps) {
-    const [modalVisible, setModalVisible] = useState(false);
-    const toggleModal = () => {
-        setModalVisible(!modalVisible);
-    };
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={toggleModal}>
+        <Text style={styles.buttonText}>🎨 Mode</Text>
+      </TouchableOpacity>
 
-    return (
-        <View>
-            <TouchableOpacity style={styles.button} onPress={toggleModal}>
-                <Text style={styles.buttonText}>🎨 Mode</Text>
+      <Modal
+        animationType="slide"
+        transparent
+        visible={modalVisible}
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Select a Colorblind Mode</Text>
+
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Deuteranopia selected');
+                toggleModal();
+              }}
+            >
+              <Text style={styles.optionText}>🟢 Deuteranopia (Red-Green)</Text>
             </TouchableOpacity>
 
-            <Modal
-                animationType="slide"
-                transparent
-                visible={modalVisible}
-                onRequestClose={toggleModal}
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Protanopia selected');
+                toggleModal();
+              }}
             >
-                <View style={styles.modalOverlay}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>Select a Colorblind Mode</Text>
+              <Text style={styles.optionText}>🔴 Protanopia (Red-Green)</Text>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                console.log('Tritanopia selected');
+                toggleModal();
+              }}
+            >
+              <Text style={styles.optionText}>🔵 Tritanopia (Blue-Yellow)</Text>
+            </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Default selected');
-                                setColorblindTheme('');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🟠 Default </Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Deuteranopia selected');
-                                setColorblindTheme('deuteranopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🟢 Deuteranopia (Red-Green)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Protanopia selected');
-                                setColorblindTheme('protanopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🔴 Protanopia (Red-Green)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={styles.optionButton}
-                            onPress={() => {
-                                console.log('Tritanopia selected');
-                                setColorblindTheme('tritanopia');
-                                toggleModal();
-                            }}
-                        >
-                            <Text style={styles.optionText}>🔵 Tritanopia (Blue-Yellow)</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            style={[styles.optionButton, {backgroundColor: 'black'}]}
-                            onPress={toggleModal}
-                        >
-                            <Text style={[styles.optionText, {color: 'white'}]}>
-                                Cancel
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
+            <TouchableOpacity
+              style={[styles.optionButton, { backgroundColor: 'black' }]}
+              onPress={toggleModal}
+            >
+              <Text style={[styles.optionText, { color: 'white' }]}>
+                Cancel
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    );
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'rgba(34, 34, 34, 0.992)',
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderRadius: 8,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        width: '80%',
-        backgroundColor: 'rgba(34, 34, 34, 0.992)',
-        padding: 20,
-        borderRadius: 12,
-        alignItems: 'center',
-    },
-    modalTitle: {
-        color: 'white',
-        fontSize: 18,
-        fontWeight: '600',
-        marginBottom: 12,
-    },
-    optionButton: {
-        backgroundColor: '#8E8E8E',
-        padding: 10,
-        borderRadius: 8,
-        marginTop: 8,
-        width: '100%',
-        alignItems: 'center',
-    },
-    optionText: {
-        color: 'white',
-    },
+  button: {
+    backgroundColor: 'rgba(34, 34, 34, 0.992)',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: 'rgba(34, 34, 34, 0.992)',
+    padding: 20,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 12,
+  },
+  optionButton: {
+    backgroundColor: '#8E8E8E',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 8,
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
+    color: 'white',
+  },
 });
