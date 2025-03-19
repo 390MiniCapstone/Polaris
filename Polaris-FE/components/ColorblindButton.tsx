a1e2f2b0ebfca9f4852584c0c14dc98ec2b8a2f3
mariabalanjian <maria.balanjian@gmail.com>
2025-03-18 21:36:57 -0400

    Refactor ColorblindButton to use external style

--- Polaris-FE/components/ColorblindButton.tsx
+++ Polaris-FE/components/ColorblindButton.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import useTheme from '@/hooks/useTheme';
 import { CustomTheme, themes } from '@/utils/themeOptions';
+import styles from './Styles/ColorblindButton';
 
 interface ThemeOptionProps {
   themeKey: string;
@@ -82,52 +83,3 @@ export function ColorblindButton() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'rgba(34, 34, 34, 0.992)',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  modalOverlay: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContent: {
-    width: '80%',
-    backgroundColor: 'rgba(34, 34, 34, 0.992)',
-    padding: 20,
-    borderRadius: 12,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 12,
-  },
-  optionButton: {
-    backgroundColor: '#8E8E8E',
-    padding: 10,
-    borderRadius: 8,
-    marginTop: 8,
-    width: '100%',
-    alignItems: 'center',
-  },
-  optionText: {
-    color: 'white',
-  },
-  cancelButton: {
-    backgroundColor: 'black',
-  },
-  cancelText: {
-    color: 'white',
-  },
-});
