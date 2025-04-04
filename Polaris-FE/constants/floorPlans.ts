678480af104ba81f177b8b6ced4ef70d59983a64
Teodor Oprea <teodor4321@gmail.com>
2025-04-03 21:14:31 -0400

    Added edges for H2 plan

--- Polaris-FE/constants/floorPlans.ts
+++ Polaris-FE/constants/floorPlans.ts
@@ -227,7 +227,143 @@ export const FLOOR_PLANS: Record<string, FloorPlanObject[]> = {
         new NodeNav('H123', 0.25, 0.29, 'hallway'),
         new NodeNav('H124', 0.25, 0.22, 'hallway'),
       ],
-      edges: [],
+      edges: [
+        ['H0', 'H1', 'hallway'],  // ? escalator?
+        ['H1', 'H2', 'hallway'],
+        ['H2', 'H3', 'hallway'],
+        ['H3', 'H4', 'hallway'],  // ? elevator?
+        ['H3', 'H5', 'hallway'],
+        ['H5', 'H6', 'hallway'],  // ? elevator?
+        ['H5', 'H7', 'hallway'],
+        ['H7', 'H8', 'hallway'],
+        ['H8', 'H9', 'hallway'],
+        ['H9', 'H10', 'hallway'],
+        ['H10', 'H11', 'hallway'],
+        ['H11', '224-1', 'hallway'],  // room
+        ['H10', 'H13', 'hallway'],
+        ['H13', 'H14', 'hallway'],
+        ['H14', '224', 'hallway'],  // room
+        ['H13', 'H16', 'hallway'],
+        ['H16', 'H17', 'hallway'],
+        ['H17', 'H18', 'hallway'],
+        ['H18', 'H19', 'hallway'],
+        ['H19', 'H20', 'hallway'],
+        ['H20', 'H21', 'hallway'],  // ? stairs?
+        ['H21', 'H22', 'hallway'],
+        ['H22', 'H23', 'hallway'],
+        ['H23', '290', 'hallway'],  // room
+        ['290', 'H35', 'hallway'],  // room, stairs?
+        ['H35', 'H36', 'hallway'],
+        ['290', 'H37', 'hallway'],  // room, stairs?
+        ['H37', 'H38', 'hallway'],
+        ['H39', 'H40', 'hallway'],
+        ['H40', '290-1', 'hallway'],  // room
+        ['H39', 'H42', 'hallway'],
+        ['H42', 'H43', 'hallway'],
+        ['H43', '205', 'hallway'],  // room
+        ['H22', 'H29', 'hallway'],
+        ['H29', 'H30', 'hallway'],
+        ['H30', 'H31', 'hallway'],  // stairs?
+        ['H31', '283', 'hallway'],  // room
+        ['283', 'H33', 'hallway'],  // room, stairs?
+        ['H22', 'H24', 'hallway'],
+        ['H24', '225', 'hallway'],  // room
+        ['225', 'H26', 'hallway'],  // room
+        ['H26', 'H27', 'hallway'],
+        ['H27', 'H28', 'hallway'],  // stairs?
+        ['H19', 'H45', 'hallway'],
+        ['H45', '213-90', 'hallway'],  // room
+        ['213-90', 'H47', 'hallway'],  // room
+        ['H47', 'H48', 'hallway'],
+        ['H48', 'H49', 'hallway'],
+        ['H49', '231-00', 'hallway'],  // room
+        ['231-00', 'H51', 'hallway'],  // room
+        ['H49', 'H52', 'hallway'],
+        ['H52', 'H53', 'hallway'],
+        ['H53', '213-90', 'hallway'],  // room
+        ['H53', 'H54', 'hallway'],
+        ['H54', 'H55', 'hallway'],
+        ['H55', 'H56', 'hallway'],
+        ['H56', 'H57', 'hallway'],
+        ['H53', 'H58', 'hallway'],
+        ['H58', 'H59', 'hallway'],
+        ['H58', 'H60', 'hallway'],
+        ['H60', '235', 'hallway'],  // room
+        ['235', 'H62', 'hallway'],  // room, stairs?
+        ['H16', 'H63', 'hallway'],
+        ['H16', 'H64', 'hallway'],
+        ['H64', 'H65', 'hallway'],
+        ['H65', 'H66', 'hallway'],
+        ['H66', 'H67', 'hallway'],
+        ['H67', 'H68', 'hallway'],
+        ['H68', '239-00', 'hallway'],  // room
+        ['239-00', 'H70', 'hallway'],  // room
+        ['H70', 'H71', 'hallway'],
+        ['H71', 'H72', 'hallway'],
+        ['H72', 'H73', 'hallway'],
+        ['H64', 'H74', 'hallway'],
+        ['H74', 'H75', 'hallway'],
+        ['H75', 'H76', 'hallway'],
+        ['H75', 'H77', 'hallway'],
+        ['H77', 'H78', 'hallway'],
+        ['H77', '222-01', 'hallway'],  // room
+        ['H78', '222-000', 'hallway'],  // room
+        ['H77', 'H81', 'hallway'],
+        ['H81', 'H82', 'hallway'],
+        ['H82', 'H83', 'hallway'],
+        ['H83', '281', 'hallway'],  // room
+        ['H82', '280', 'hallway'],  // room
+        ['H81', 'H86', 'hallway'],
+        ['H86', 'H87', 'hallway'],
+        ['H87', '275', 'hallway'],  // room?  // todo: 275 is a stairway
+        ['H87', 'H89', 'hallway'],
+        ['H86', 'H90', 'hallway'],
+        ['H90', 'H91', 'hallway'],
+        ['H91', 'H92', 'hallway'],
+        ['H91', 'H96', 'hallway'],
+        ['H96', 'H95', 'hallway'],
+        ['H96', 'H98', 'hallway'],
+        ['H98', 'H97', 'hallway'],
+        ['H98', 'H100', 'hallway'],
+        ['H100', 'H99', 'hallway'],
+        ['H100', 'H102', 'hallway'],
+        ['H102', 'H101', 'hallway'],
+        ['H102', 'H104', 'hallway'],
+        ['H104', 'H106', 'hallway'],
+        ['H106', 'H105', 'hallway'],
+        ['H106', 'H108', 'hallway'],
+        ['H108', 'H109', 'hallway'],
+        ['H108', 'H107', 'hallway'],
+        ['H107', 'H105', 'hallway'],
+        ['H105', 'H103', 'hallway'],
+        ['H103', 'H101', 'hallway'],
+        ['H101', 'H99', 'hallway'],
+        ['H99', 'H97', 'hallway'],
+        ['H97', 'H95', 'hallway'],
+        ['H95', 'H92', 'hallway'],
+        ['H92', 'H93', 'hallway'],
+        ['H93', 'H94', 'hallway'],
+        ['H94', 'H77', 'hallway'],
+        ['H107', 'H122', 'hallway'],
+        ['H122', 'H124', 'hallway'],  // escalators?
+        ['H122', 'H123', 'hallway'],
+        ['H123', 'H2', 'hallway'],
+        ['H5', 'H110', 'hallway'],
+        ['H110', 'H111', 'hallway'],
+        ['H110', 'H112', 'hallway'],
+        ['H112', 'H114', 'hallway'],
+        ['H114', 'H116', 'hallway'],
+        ['H116', 'H118', 'hallway'],
+        ['H118', 'H120', 'hallway'],
+        ['H120', 'H121', 'hallway'],
+        ['H121', 'H119', 'hallway'],
+        ['H119', 'H117', 'hallway'],
+        ['H117', 'H115', 'hallway'],
+        ['H115', 'H113', 'hallway'],
+        ['H113', 'H111', 'hallway'],
+        ['H111', 'H7', 'hallway'],
+        ['H121', 'H74', 'hallway'],
+      ],
     },
     {
       SvgComponent: H8,
