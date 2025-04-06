e08099daa91da917b4dac2f4278c59a4c4c5c71e
Mahmoud Mohamed <mahmoud@null.net>
2025-04-06 04:49:54 -0400

    Modified NavigationPolyline.tsx to display different polylines for shuttle bus navigation

--- Polaris-FE/components/Navigation/NavigationPolyline.tsx
+++ Polaris-FE/components/Navigation/NavigationPolyline.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Marker, Polyline } from 'react-native-maps';
+import { Polyline } from 'react-native-maps';
 import { useNavigation } from '@/contexts/NavigationContext/NavigationContext';
 import useTheme from '@/hooks/useTheme';
 
@@ -10,44 +10,83 @@ export const NavigationPolyline: React.FC = () => {
     snappedPoint,
     clippedPolyline,
     navigationState,
+    shuttleData,
+    nextDeparture,
   } = useNavigation();
   const { theme } = useTheme();
 
-  return (
-    <React.Fragment>
-      {(navigationState === 'planning' || navigationState === 'navigating') &&
-        clippedPolyline &&
-        snappedPoint &&
-        destination && (
-          <React.Fragment>
-            <Marker
-              key={theme.colors.primary}
-              coordinate={destination}
-              pinColor={theme.colors.primary}
+  if (
+    !(navigationState === 'planning' || navigationState === 'navigating') ||
+    !clippedPolyline ||
+    !snappedPoint ||
+    !destination
+  ) {
+    return null;
+  }
+
+  const polyline = () => {
+    if (travelMode === 'WALK') {
+      return (
+        <Polyline
+          coordinates={clippedPolyline}
+          strokeWidth={4}
+          strokeColor={theme.colors.primary}
+          lineDashPattern={[5, 10]}
+        />
+      );
+    } else if (travelMode === 'SHUTTLE') {
+      if (
+        nextDeparture &&
+        shuttleData?.legOne &&
+        shuttleData?.legTwo &&
+        shuttleData?.legThree
+      ) {
+        return (
+          <>
+            <Polyline
+              coordinates={shuttleData.legOne.polyline}
+              strokeWidth={4}
+              strokeColor={theme.colors.primary}
+              lineDashPattern={[5, 10]}
             />
-            {travelMode === 'WALK' ? (
-              <Polyline
-                coordinates={clippedPolyline}
-                strokeWidth={4}
-                strokeColor={theme.colors.primary}
-                lineDashPattern={[5, 10]}
-              />
-            ) : (
-              <React.Fragment>
-                <Polyline
-                  coordinates={clippedPolyline}
-                  strokeWidth={9}
-                  strokeColor={theme.colors.primary}
-                />
-                <Polyline
-                  coordinates={clippedPolyline}
-                  strokeWidth={6}
-                  strokeColor={theme.colors.secondary}
-                />
-              </React.Fragment>
-            )}
-          </React.Fragment>
-        )}
-    </React.Fragment>
-  );
+            <Polyline
+              coordinates={shuttleData.legTwo.busData.polyline}
+              strokeWidth={9}
+              strokeColor={theme.colors.primary}
+            />
+            <Polyline
+              coordinates={shuttleData.legTwo.busData.polyline}
+              strokeWidth={6}
+              strokeColor={theme.colors.secondary}
+            />
+            <Polyline
+              coordinates={shuttleData.legThree.polyline}
+              strokeWidth={4}
+              strokeColor={theme.colors.primary}
+              lineDashPattern={[5, 10]}
+            />
+          </>
+        );
+      } else {
+        return null;
+      }
+    } else {
+      return (
+        <>
+          <Polyline
+            coordinates={clippedPolyline}
+            strokeWidth={9}
+            strokeColor={theme.colors.primary}
+          />
+          <Polyline
+            coordinates={clippedPolyline}
+            strokeWidth={6}
+            strokeColor={theme.colors.secondary}
+          />
+        </>
+      );
+    }
+  };
+
+  return <>{polyline()}</>;
 };
